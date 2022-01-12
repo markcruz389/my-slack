d4bb9abeafde23d03ca0ed79e277e9a22f96173f
markcruz389 <markanthonycruz1230@gmail.com>
2022-01-13 00:51:00 +0800

    Adds channel list

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -3,7 +3,7 @@ import ChannelModal from "./ChannelModal";
 import { saveChannel } from "../../api/channelsApi";
 import { getUsers } from "../../api/usersApi";
 import { toast } from "react-toastify";
-import { UserLoginContext } from "../context";
+import { UserLoginContext, ChannelsContext } from "../context";
 
 const Sidebar = () => {
   const [showModal, setShowModal] = useState(false);
@@ -13,6 +13,7 @@ const Sidebar = () => {
   });
   const [channelErrors, setChannelErrors] = useState({});
   const [userLoginContext] = useContext(UserLoginContext);
+  const channels = useContext(ChannelsContext);
 
   useEffect(() => {});
 
@@ -59,11 +60,27 @@ const Sidebar = () => {
   return (
     <>
       <div className='row pt-5'>
-        <div className='col-2'>
+        {/* <div className='col-2'>
           <button className='btn btn-info btn-sm'>&#9660;</button>
-        </div>
-        <div className='col-8 d-flex align-items-center'>
-          <span className='align-middle'>Channels</span>
+        </div> */}
+        <div className='col-10 d-flex align-items-center'>
+          {/* <a
+            className='align-middle'
+            href='#collapseExample'
+            data-bs-toggle='collapse'role="button"
+          >
+            Channels
+          </a> */}
+          <a
+            className='btn btn-primary btn-sm'
+            data-bs-toggle='collapse'
+            href='#channelsCollapse'
+            role='button'
+            aria-expanded='false'
+            aria-controls='channelsCollapse'
+          >
+            Channels
+          </a>
         </div>
         <div className='col-2'>
           <button
@@ -72,6 +89,16 @@ const Sidebar = () => {
           >
             +
           </button>
+        </div>
+        <div className='collapse mt-2' id='channelsCollapse'>
+          <ul className='list-group'>
+            {console.log(channels.data)}
+            {channels.data?.map((channel) => (
+              <li key={channel.id} className='list-group-item'>
+                {channel.name}
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
       <ChannelModal
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
 import { initLocalStorage } from "./services/localStorage";
 
 initLocalStorage();
